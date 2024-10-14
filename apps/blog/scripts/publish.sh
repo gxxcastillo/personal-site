ba8e142ecf9c4b2e2a080653cfdf23deccaf9518
Gabriel <g@brielcastillo.me>
2024-10-14 02:46:17 -0700

    sync non-html files

--- apps/blog/scripts/publish.sh
+++ apps/blog/scripts/publish.sh
@@ -18,5 +18,8 @@ pnpm nx build blog
 echo -e "\nStripping .html file extensions"
 find . -type f -name "*.html" | while read file; do mv "$file" "${file%.html}"; done
 
-echo -e "\nSyncing files to S3..."
+echo -e "\nSyncing html files to S3..."
 aws s3 sync "$BUILD_PATH" "s3://$S3_BUCKET_NAME" --metadata-directive REPLACE --content-type "text/html" --exclude "*.*"
+
+echo -e "\nSyncing non-html files to S3..."
+aws s3 sync "$BUILD_PATH" "s3://$S3_BUCKET_NAME" --include "*.*"
